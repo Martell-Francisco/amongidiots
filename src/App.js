516e89cbbe2bdbdf685083c40756f030816e6354
MARTELL-FRANCISCO <franciscomartell@att.net>
2024-01-24 21:52:57 -0600

    Changed Routes System

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ import Calender from './pages/Calender';
 import SecretSignUpPage from './pages/SecretPage';
 
 function App() {
-  return <Routes>
+  return (<Routes>
       <Route path="/" element={<Home/>}/>
       <Route path='tobis-theater' element={<Tobistheater />}/>
       <Route path='AssignImposter' element={<CheckImposter />}/>
       <Route path='Planner' element={<Calender/>}/>
       <Route path='SecretSignUpPage' element={<SecretSignUpPage/>}/>
 
-    </Routes>
+    </Routes>)
   
 }
 
